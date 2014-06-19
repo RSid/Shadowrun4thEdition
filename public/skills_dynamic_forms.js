4d58615ec3130d3531a8ebce1cefc54127d83588
RSid <sm1th303@gmail.com>
2014-06-19 15:49:06 -0400

    Dynamic menu form for general gear exists, and shows/disappears at correct moments base don dropdown

--- public/skills_dynamic_forms.js
+++ public/skills_dynamic_forms.js
@@ -1,18 +1,25 @@
 $( document ).ready(function() {
   $('#weapons-input').hide();
+  $('#general-gear-input').hide();
 
   $('#PickGear').change(function() {
 
     var optionValue = $('#PickGear option:selected').val();
 
-    if ( optionValue!=1 ) {
-
-      $('#weapons-input').hide();
-      $('#under-constructon').show();
-
-    } else {
+    if ( optionValue==1 ) {
 
       $('#weapons-input').show();
+      $('#general-gear-input').hide();
+      $('#under-constructon').show();
+
+    } else if ( optionValue==4 ){
+        $('#weapons-input').hide();
+        $('#general-gear-input').show();
+    }
+
+    else {
+      $('#weapons-input').hide();
+      $('#general-gear-input').hide();
       $('#under-constructon').show();
     }
   });
